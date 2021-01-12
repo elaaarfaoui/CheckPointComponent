6aacf468ce2c60c5281a03576833735608f6d40c
elaaarfaoui <elaa.arfaoui1@gmail.com>
2021-01-12 18:30:00 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+             <h4 style={{color:" black"}}>1 Rue Iben Hani Andaloussi Hammam Chatt</h4> 
+        </div>
+    )
+}   
+
+export default Address
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  image  from "../../img.jpg";
+
+const ProfilPhoto = () => {
+    return (
+        <img src={image} alt="Profile pic"/>    
+    )
+}
+
+export default ProfilPhoto;
